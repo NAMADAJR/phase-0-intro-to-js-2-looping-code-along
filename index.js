4a79a9c13f83e7ceb8d6677192889c4542d9d703
Namada Junior <namada871@gmail.com>
2024-06-21 12:57:21 +0300

    commit

--- index.js
+++ index.js
@@ -2,21 +2,16 @@
 
 const names =("Guadalupe","Ollie","Aki")
 
-
-let countup = 0;
-while (countup < 10) {
-  console.log(countup++);
-}
-
-
-for (let countup = 0; countup < 10; countup++) {
-  console.log(countup);
-}
-
 function writeCards(names) {
     let messages = [];
     for (let i = 0; i < names.length; i++) {
         messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
     }
     return messages;
+}
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
 }
